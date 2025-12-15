7a1c3e3c5120c61ddabb00a87865be5d47c237e1
Mark Tasaka <mark.tasaka@gmail.com>
2025-12-14 21:54:07 -0800

    dccOccupations.js to store occupation data

--- src/utils/lv0DccGenerator.js
+++ src/utils/lv0DccGenerator.js
@@ -1,22 +1,8 @@
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import characterSheetBg from '../character/img/lvZeroCharacterSheet.png';
+import { occupations } from './dccOccupations.js';
 
 // Character generation data
-const occupations = [
-  'Alchemist', 'Animal trainer', 'Armorer', 'Astrologer', 'Barber', 'Beadle', 'Beekeeper', 'Blacksmith',
-  'Butcher', 'Caravan guard', 'Cheesemaker', 'Cobbler', 'Confidence artist', 'Cooper', 'Costermonger',
-  'Cutpurse', 'Ditch digger', 'Dwarven apothecarist', 'Dwarven blacksmith', 'Dwarven chest-maker',
-  'Dwarven herder', 'Dwarven miner', 'Dwarven mushroom-farmer', 'Dwarven rat-catcher', 'Dwarven stonemason',
-  'Elven artisan', 'Elven barrister', 'Elven chandler', 'Elven falconer', 'Elven forester', 'Elven glassblower',
-  'Elven navigator', 'Elven sage', 'Farmer', 'Fortune teller', 'Gambler', 'Gong farmer', 'Grave digger',
-  'Guild beggar', 'Halfling chicken butcher', 'Halfling dyer', 'Halfling glovemaker', 'Halfling gypsy',
-  'Halfling haberdasher', 'Halfling healer', 'Halfling moneylender', 'Halfling trader', 'Healer',
-  'Herbalist', 'Herder', 'Hunter', 'Indentured servant', 'Jester', 'Jeweler', 'Locksmith', 'Mendicant',
-  'Merchant', 'Miller', 'Minstrel', 'Noble', 'Orphan', 'Ostler', 'Outlaw', 'Rope maker', 'Sailor',
-  'Scribe', 'Shaman', 'Slave', 'Smuggler', 'Soldier', 'Squire', 'Tax collector', 'Trapper', 'Urchin',
-  'Wainwright', 'Weaver', 'Wizard\'s apprentice', 'Woodcutter'
-];
-
 const alignments = ['Lawful', 'Neutral', 'Chaotic'];
 const genders = ['Male', 'Female'];
 
