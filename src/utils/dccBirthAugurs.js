3349fdc04d64dea78a7533a75a784374aedde809
Mark Tasaka <mark.tasaka@gmail.com>
2025-12-14 22:41:47 -0800

    dccBirthAugurs.js added for birth augur data

---
+++ src/utils/dccBirthAugurs.js
@@ -0,0 +1,11 @@
+export const birthAugurs = [
+  'Harsh winter: All attack rolls', 'The bull: Melee attack rolls', 'Fortunate date: Missile attack rolls',
+  'Raised by wolves: Unarmed attack rolls', 'Conceived on horseback: Mounted attack rolls',
+  'Born on the battlefield: Damage rolls', 'Path of the bear: Melee damage rolls',
+  'Hawkeye: Missile damage rolls', 'Pack hunter: Attack and damage rolls for 0-level starting weapon',
+  'Born under the loom: Skill checks (including thief skills)', 'Fox\'s cunning: Find/disable traps',
+  'Four-leafed clover: Find secret doors', 'Seventh son: Spell checks', 'The raging storm: Spell damage',
+  'Righteous heart: Turn unholy checks', 'Survived the plague: Magical healing',
+  'Lucky sign: Saving throws', 'Guardian angel: Turn unholy checks', 'Survived war: Initiative and attacks',
+  'Lucky bone: Find/disable traps and magical healing'
+];
--- src/utils/lv0DccGenerator.js
+++ src/utils/lv0DccGenerator.js
@@ -1,6 +1,7 @@
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import characterSheetBg from '../character/img/lvZeroCharacterSheet.png';
 import { occupations } from './dccOccupations.js';
+import { birthAugurs } from './dccBirthAugurs.js';
 
 // Character generation data
 const alignments = ['Lawful', 'Neutral', 'Chaotic'];
@@ -12,18 +13,6 @@ const names = {
   female: ['Adrie', 'Ahanna', 'Aramara', 'Aranea', 'Berris', 'Caelynn', 'Carric', 'Dayereth', 'Enna', 'Galinndan',
            'Hadarai', 'Halimath', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Korfel', 'Lada', 'Lamlis', 'Wade']
 };
-
-const birthAugurs = [
-  'Harsh winter: All attack rolls', 'The bull: Melee attack rolls', 'Fortunate date: Missile attack rolls',
-  'Raised by wolves: Unarmed attack rolls', 'Conceived on horseback: Mounted attack rolls',
-  'Born on the battlefield: Damage rolls', 'Path of the bear: Melee damage rolls',
-  'Hawkeye: Missile damage rolls', 'Pack hunter: Attack and damage rolls for 0-level starting weapon',
-  'Born under the loom: Skill checks (including thief skills)', 'Fox\'s cunning: Find/disable traps',
-  'Four-leafed clover: Find secret doors', 'Seventh son: Spell checks', 'The raging storm: Spell damage',
-  'Righteous heart: Turn unholy checks', 'Survived the plague: Magical healing',
-  'Lucky sign: Saving throws', 'Guardian angel: Turn unholy checks', 'Survived war: Initiative and attacks',
-  'Lucky bone: Find/disable traps and magical healing'
-];
 
 // Complete PDF Styles
 const styles = StyleSheet.create({
