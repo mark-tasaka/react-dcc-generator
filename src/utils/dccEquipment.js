e4e5414d91b1dfb5c7b238123f4d2ad5aa1c01f1
Mark Tasaka <mark.tasaka@gmail.com>
2025-12-16 22:05:57 -0800

    dccEquipment added

---
+++ src/utils/dccEquipment.js
@@ -0,0 +1,35 @@
+export const getRandomEquipment = () => {
+    const equipment = [
+        "backpack",
+        "candle",
+        "chain (10 feet)",
+        "chalk (1 piece)",
+        "chest (empty)",
+        "crowbar",
+        "flask (empty)",
+        "flint and steel",
+        "grappling hook",
+        "small hammer",
+        "holy symbol",
+        "holy water (1 vial)",
+        "iron spike",
+        "lantern",
+        "mirror (hand size)",
+        "oil (1 flask)",
+        "10 foot Pole",
+        "ration (1 day)",
+        "rope (50 feet)",
+        "sack (large)",
+        "sack (small)",
+        "thieves tools",
+        "torch (1)",
+        "waterskin"
+    ];
+
+    // Generate random index and return the equipment at that index
+    const randomIndex = Math.floor(Math.random() * equipment.length);
+    return equipment[randomIndex];
+};
+
+// Default export if you prefer
+export default getRandomEquipment;
